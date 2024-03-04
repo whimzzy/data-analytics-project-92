e4190358a13e217886dbc21de3fc3ec5f8a00ff0
whimzzy <153300402+whimzzy@users.noreply.github.com>
2024-03-04 10:43:21 +0300

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -29,25 +29,20 @@ HAVING round(avg(income), 0) < (SELECT avg(income) FROM tab)
 /*выбор тех, у кого средний доход меньше общего среднего дохода*/
 ORDER BY average_income ASC;
 
-select concat(e.first_name, ' ', e.last_name) as name, 
+WITH tab AS (select concat(e.first_name, ' ', e.last_name) as name, 
 to_char(s.sale_date, 'fmday') as weekday, /*преобразуем дату в день недели*/ 
-round(sum(s.quantity*p.price), 0) as income
+round(sum(s.quantity*p.price), 0) as income, EXTRACT(isodow from s.sale_date) AS extract
 from sales s
 left join employees e 
 on s.sales_person_id = e.employee_id 
 left join products p 
 on s.product_id = p.product_id
-group by to_char(s.sale_date, 'fmday'), concat(e.first_name, ' ', e.last_name)
-order by case 
-	when to_char(s.sale_date, 'fmday') = 'monday' then 1
-	when to_char(s.sale_date, 'fmday') = 'tuesday' then 2
-	when to_char(s.sale_date, 'fmday') = 'wednesday' then 3
-	when to_char(s.sale_date, 'fmday') = 'thursday' then 4
-	when to_char(s.sale_date, 'fmday') = 'friday' then 5
-	when to_char(s.sale_date, 'fmday') = 'saturday' then 6
-	when to_char(s.sale_date, 'fmday') = 'sunday' then 7
-end
-asc, /*выставление условия для нумерации дней недели*/ name asc;
+group by to_char(s.sale_date, 'fmday'), 
+concat(e.first_name, ' ', e.last_name), EXTRACT(isodow from s.sale_date)
+)
+SELECT name, weekday, income
+FROM tab
+ORDER BY extract ASC, name ASC;
 
 WITH tab AS (select case
 	when age >=16 and age <=25 then '16-25'
